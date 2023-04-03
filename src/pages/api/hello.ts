c5bf445626777ee00bb7afde326cf3d53cfe16ee
mirthis@gmail.com <mirthis@gmail.com>
2023-04-03 23:20:26 +0100

    first commit

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,13 +1,15 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
 
 type Data = {
-  name: string
-}
+  response: string;
+};
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  const prompt = req.body.prompt;
+  console.log({ req });
+  res.status(200).json({ response: `I received this promtp ${prompt}` });
 }
